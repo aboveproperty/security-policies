c6ebf26b7a805bd1f90f73f535679d829fbbdece
Pete Ehlke <pete.ehlke@aboveproperty.com>
2023-10-06 11:39:49 -0700

    chore(AdHoc): Log last commit as currentRevision

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 rm -fr ./site/* ./docs/* ./partials/*
+command -v gdate >/dev/null 2>&1 && DATE_CMD=gdate || DATE_CMD=date
+sed -i  -e "s,\"currentRevision\":.*,\"currentRevision\": \"Last Reviewed: $(git log -1 --format="%at" | xargs -I{} $DATE_CMD -d @{} +%F:%T-%Z)\"," abvprp-config.json
 psp build -t ./templates -c abvprp-config.json
 mkdocs build
 sed 's/class="md-sidebar\ /style="display: none" class="md-sidebar\ /g' site/privacy-policy/index.html > site/privacy-policy/index-standalone.html
 sed -i 's/class="md-search"/style="display: none" class="md-search"/g' site/privacy-policy/index-standalone.html
 sed -i 's/class="md-footer"/style="display: none" class="md-footer"/g' site/privacy-policy/index-standalone.html
+sed -i 's/class="md-footer"/style="display: none" class="md-header"/g' site/privacy-policy/index-standalone.html
 mkdir -p site/docx
 cat mkdocs.yml | grep -v index.md | yq -r '.nav[] | to_entries | .[] | .value' > temp.md
-cat temp.md
+#cat temp.md
 for token in $(cat mkdocs.yml | grep -v index.md | yq -r '.nav[] | to_entries | .[] | .value'); do
     sed -i "s/$token#/#/g" temp.md
 done
@@ -42,6 +45,6 @@ sed -i "s/approved-vendors\.md/\#appendix-c-approved-vendors/" temp.md
 sed -i "s/definitions\.md/\#appendix-d-key-definitions/" temp.md
 sed -i "s/privacy-policy\.md/\#appendix-e-privacy-policy/" temp.md
 sed -i "s/cookie-policy\.md/\#appendix-f-cookie-policy/" temp.md
-pandoc -s --toc -o site/docx/policies.docx temp.md
+#pandoc -s --toc -o site/docx/policies.docx temp.md
 rm temp.md
-echo "Please open $(pwd)/site/docx/policies.docx in Microsoft Word to fix the TOC."
+#echo "Please open $(pwd)/site/docx/policies.docx in Microsoft Word to fix the TOC."
